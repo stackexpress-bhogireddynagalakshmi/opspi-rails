1a06b7418e64216e5662653471436324f1bf71f6
root <naveen.tiwari@stackexpress.com>
2021-03-04 05:23:55 +0000

    added sentory.io

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,18 @@ module OpsPi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.hosts = nil
+Sentry.init do |config|
+  config.dsn = 'https://497129b41a4c47689ca484b9a31a73f1@o541736.ingest.sentry.io/5660861'
+  config.breadcrumbs_logger = [:active_support_logger]
+
+  # To activate performance monitoring, set one of these options.
+  # We recommend adjusting the value in production:
+  config.traces_sample_rate = 0.5
+  # or
+  config.traces_sampler = lambda do |context|
+    true
+  end
+end
 
     # config.assets.precompile += %w( store/all.js store/all.css admin/all.js admin/all.css)
 
