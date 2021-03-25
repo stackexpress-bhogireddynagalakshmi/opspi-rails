44d1e982a135385ee05493d48ef25c763274244b
Syed Shibli <syed.m.shibli@et.apyl.com>
2021-03-25 10:47:35 +0530

    formatting

--- app/lib/solid_cp/config.rb
+++ app/lib/solid_cp/config.rb
@@ -2,7 +2,6 @@ module SolidCp
  	class Config
     class_attribute :username, :password, :base_url, :register_users,:log,:timeout
     # Load yaml settings
-    ActiveRecord::Base.establish_connection( )
     YAML.load( ERB.new( File.read( "#{Rails.root}/config/solid_cp.yml" )).result)[Rails.env].each do |key, value|
       self.send("#{key}=", value)
     end
