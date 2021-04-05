d091f87d2b18e05f8f0638ba90d0ff3ae0678867
Syed Shibli <syed.m.shibli@et.apyl.com>
2021-04-05 13:32:24 +0530

    Redis endpoint changes

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://127.0.0.1:6379/5' }
+  config.redis = { url: 'redis://139.59.18.196:6379/5' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://127.0.0.1:6379/5' }
+  config.redis = { url: 'redis://139.59.18.196:6379/5' }
 end
