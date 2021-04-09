51d4fa6d78a3b09f4225451497edd89529911e80
root <naveen.tiwari@stackexpress.com>
2021-04-09 07:51:08 +0000

    added volumes for docker log and redish private ip in sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,6 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://139.59.18.196:6379/5' }
+  config.redis = { url: 'redis://10.122.0.2:6379/5' }
 end
-
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://139.59.18.196:6379/5' }
+  config.redis = { url: 'redis://10.122.0.2:6379/5' }
 end
