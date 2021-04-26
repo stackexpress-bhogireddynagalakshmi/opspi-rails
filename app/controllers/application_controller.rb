5ef11d2ea5ad061f9d2a35a9dad021d98e9a2b28
Syed Shibli <syed.m.shibli@et.apyl.com>
2021-04-26 13:35:38 +0530

    Isues Fixes for Solid CP flow etc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,19 @@ class ApplicationController < ActionController::Base
 	 set_current_tenant_by_subdomain_or_domain(:account, :subdomain,:domain)
 
 	 before_action do
+	 	# byebug
 	 	 if DomainCheck.check_domain(request)
-	 		ActsAsTenant.current_tenant = Account.where('subdomain = ? or domain = ?',request.host,request.host).first unless current_spree_user.present? && current_spree_user.superadmin?
+	 	 	#unless current_spree_user.present? && current_spree_user.superadmin?
+	 		ActsAsTenant.current_tenant = Account.where('subdomain = ? or domain = ?',request.host,request.host).first 
+	 		ActsAsTenant.current_tenant = nil if current_spree_user&.superadmin?
 	 	 else
 	 	 	render :json=>'Sorry, this domain is currently unavailable.'
 	 	 end
 	 end
+
+
+
+
 
 	private
 
---
+++ app/models/spree/line_item_decorator.rb
@@ -0,0 +1,12 @@
+module Spree
+	module LineItemDecorator
+		
+		def ensure_valid_quantity
+	      	self.quantity = 1 if self.quantity !=1
+	    end
+
+	end
+end
+
+
+::Spree::LineItem.prepend Spree::LineItemDecorator if ::Spree::LineItem.included_modules.exclude?(Spree::LineItemDecorator)
