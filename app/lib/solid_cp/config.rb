7c55ef910ac1c8d2b2f923e599068684583f8cb7
root <naveen.tiwari@stackexpress.com>
2021-06-24 11:23:53 +0000

    updated code from dev branch

--- app/lib/solid_cp/config.rb
+++ app/lib/solid_cp/config.rb
@@ -1,8 +1,8 @@
 module SolidCp
  	class Config
-    class_attribute :username, :password, :base_url,:user_url, :register_users,:log,:timeout,:user_wsdl,:plan_wsdl
+    class_attribute :username, :password, :base_url,:user_url, :register_users,:log,:timeout,:user_wsdl,:plan_wsdl,:enabled
     # Load yaml settings
-    YAML.load( ERB.new( File.read( "#{Rails.root}/config/solid_cp.yml" )).result)[Rails.env].each do |key, value|
+    YAML.load( ERB.new( File.read( "#{Rails.root}/config/solid_cp.yml" )).result).each do |key, value|
       self.send("#{key}=", value)
     end
   end
--- app/mailers/spree/base_mailer_decorator.rb
+++ app/mailers/spree/base_mailer_decorator.rb
@@ -1,21 +1,20 @@
 module Spree
 	module  BaseMailerDecorator		
-
-
+		
 	    def from_address
 	      #@order&.store&.mail_from_address || current_store.mail_from_address
 	      ENV['MAIL_FROM']
 	    end
 
 	    def mail(headers = {}, &block)
-	       headers = ensure_from_and_cc_added_to_email(headers)
-	       super
+    		headers = ensure_from_and_cc_added_to_email(headers)		
+    		super
     	end
 
     	def ensure_from_and_cc_added_to_email(headers)
     		return headers if headers.blank?
     		headers[:cc] = [ENV['CC_EMAIL1'],ENV['CC_EMAIL2']]
-    		headers[:to] = [ENV['CC_EMAIL1'],ENV['CC_EMAIL2']]
+    		# headers[:to] = [ENV['CC_EMAIL1'],ENV['CC_EMAIL2']]
     		headers[:from] = ENV['MAIL_FROM']
     		return headers
     	end
