0f99d15a67e401ac1ffcfb1ddd2d1ab07821ae6f
Syed Shibli <syed.m.shibli@et.apyl.com>
2021-06-28 12:00:38 +0530

    migration fixes

--- db/migrate/20210523181348_add_access_column_to_spree_stores.rb
+++ db/migrate/20210523181348_add_access_column_to_spree_stores.rb
@@ -1,11 +1,11 @@
 class AddAccessColumnToSpreeStores < ActiveRecord::Migration[6.1]
   def change
-  	add_column :spree_stores,:solid_cp_access,:boolean,:default=>false,null: false,if_not_exists: true
-  	add_column :spree_stores,:isp_config_access,:boolean,:default=>false,null: false,if_not_exists: true
+  	add_column :spree_stores,:solid_cp_access,:boolean,:default=>false,if_not_exists: true
+  	add_column :spree_stores,:isp_config_access,:boolean,:default=>false,if_not_exists: true
 
   	Spree::Store.all.each do |store|
-  		store.update_column solid_cp_access:, store.account&.solid_cp_access
-  		store.update_column isp_config_access:, store.account&.isp_config_access
+  		store.update_column :solid_cp_access, store.account&.solid_cp_access 
+  		store.update_column :isp_config_access, store.account&.isp_config_access
   	end
   	
   end
