c776d89a81b35a9cf40b0ec0783d1acae949031f
dell <syed.m.shibli@et.apyl.com>
2021-06-30 12:44:54 +0530

    git resolve

--- app/services/tenant_manager/store_tenant_updater.rb
+++ app/services/tenant_manager/store_tenant_updater.rb
@@ -1,32 +1,16 @@
 module TenantManager
   class StoreTenantUpdater < ApplicationService
-<<<<<<< HEAD
-  	attr_reader :store,:tenant_id
-  		
-		def initialize(store,tenant_id=nil)
-			@store = store
-      @tenant_id = tenant_id
-		end
-
-		def call
-			ActsAsTenant.without_tenant do
-        store.update_column :account_id, tenant_id
-		  end
-    end
-    
-=======
-  		attr_reader :store,:tenant_id
-  		
-  		def initialize(store,tenant_id=nil)
-  			@store = store
+    attr_reader :store,:tenant_id
+      
+      def initialize(store,tenant_id=nil)
+        @store = store
         @tenant_id = tenant_id
-  		end
-
-  		def call
-  			 ActsAsTenant.without_tenant do
-           store.update_column :account_id, tenant_id
-			   end
       end
->>>>>>> c9edcc25c01a60f80fbc6a108ca1b684916a62fa
+
+      def call
+         ActsAsTenant.without_tenant do
+           store.update_column :account_id, tenant_id
+         end
+      end
  	end
 end
