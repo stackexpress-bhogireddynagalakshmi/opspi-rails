2dba6247fd81b9336169d7aa28d591a4268e7a1a
dell <syed.m.shibli@et.apyl.com>
2021-07-13 10:50:18 +0530

    Changes to the Pricing Tabs + Removed First Name and Last Name from Signup etc

--- app/controllers/hosting_controller.rb
+++ app/controllers/hosting_controller.rb
@@ -2,22 +2,15 @@ class HostingController < Spree::StoreController
 	# layout 'spree/layouts/spree_application'
 	include Spree::CacheHelper
 
-	def index
-		
-	end
-
 	def servers
 		@products = current_store.try(:account).try(:spree_products) 
 		if params[:slug] == 'vps'
-			render 'servers'
 		elsif params[:slug] == 'linux-servers'
 			@products = @products.linux
-			render 'servers'
 		elsif params[:slug] == 'windows-servers'
 			@products = @products.windows
-			render 'servers'
-		else
-			render 'default'
 		end	
+
+		render 'servers'
 	end
 end
