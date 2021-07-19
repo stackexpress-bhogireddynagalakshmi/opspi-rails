9e7fdd5c1b8c4f7fe4d8b7f7214134af79470eee
dell <syed.m.shibli@et.apyl.com>
2021-07-19 11:12:23 +0530

    Devise Auth Session controller fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,27 +3,12 @@ class ApplicationController < ActionController::Base
 
 	helper_method [:get_tenant_host_for_resource_path]
 
-	before_action do
-	 	if DomainCheck.check_domain(request)
-	 		ActsAsTenant.current_tenant = Account.where('subdomain = ? or domain = ?',request.host,request.host).first
-	 		ActsAsTenant.current_tenant = nil if current_spree_user&.superadmin?
-	 	else
-	 	 	render :json=>'Sorry, this domain is currently unavailable.'
-	 	end
-	end
+	include Tenantable
 
-	def get_tenant_host_for_resource_path(resource)
-	  [request.protocol,resource.account.domain,":#{request.port}"].join
-	end
+	 before_action do
+      set_tenant
+    end
 
-	private
-
-	class DomainCheck
-		def self.check_domain request
-			whitelisted_domains = Spree::Store.pluck(:url) + [OpspiHelper.admin_domain]
-		    whitelisted_domains.include? (request.host)
-		end
-	end
 
 end
 
