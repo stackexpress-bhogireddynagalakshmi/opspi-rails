b9ec3b6466360b618d9e573ad3417eb2ad978c64
Syed Shibli <syed.m.shibli@et.apyl.com>
2021-09-10 18:51:23 +0530

    Changes

--- app/controllers/spree/admin/payment_methods_controller_decorator.rb
+++ app/controllers/spree/admin/payment_methods_controller_decorator.rb
@@ -3,15 +3,25 @@ module Spree
       module PaymentMethodsControllerDecorator
 
         def index
-          @payment_methods = @payment_methods.where(account_id: TenantManager::TenantHelper.current_tenant.id)
+          @payment_methods = @payment_methods.where(account_id: tenant_id)
         end 
 
         def find_resource
           if parent_data.present?
-            parent.send(controller_name).where(account_id: TenantManager::TenantHelper.current_tenant.id).find(params[:id])
+            parent.send(controller_name).where(account_id: tenant_id).find(params[:id])
           else
             base_scope = model_class.try(:for_store, current_store) || model_class
-            base_scope.where(account_id: TenantManager::TenantHelper.current_tenant.id).find(params[:id])
+            base_scope.where(account_id: tenant_id).find(params[:id])
+          end
+        end
+
+        private
+
+        def tenant_id
+          if TenantManager::TenantHelper.current_tenant.present?
+            TenantManager::TenantHelper.current_tenant.id
+          else
+            TenantManager::TenantHelper.admin_tenant.id
           end
         end
 
