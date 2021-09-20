bf3383a12940b425b0203ab953967b095249f940
Syed Shibli <syed.m.shibli@et.apyl.com>
2021-09-20 11:11:20 +0530

    Payment Method order by position + Shipping Address hidden

--- app/models/spree/payment_method_decorator.rb
+++ app/models/spree/payment_method_decorator.rb
@@ -1,11 +1,13 @@
 module Spree
 	module PaymentMethodDecorator
-
+  
 		def self.prepended(base)
 	    #base.acts_as_tenant :account,:class_name=>'::Account'
       base.belongs_to :account,:class_name=>'::Account'
 	    base.after_commit :add_to_tenant, on: [:create,:update]
       base.after_commit :update_store_payment_methods, on: [:create,:update]
+
+      base.acts_as_list scope: [:account_id]
 	  end
 
 	  def add_to_tenant
@@ -39,5 +41,8 @@ module Spree
 end
 
 ::Spree::PaymentMethod.prepend Spree::PaymentMethodDecorator if ::Spree::Address.included_modules.exclude?(Spree::PaymentMethodDecorator)
+
+Spree::PermittedAttributes.store_attributes.push << :position
+
 
 
