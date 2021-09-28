1d229024cbc6b7e683a08b4ac4427814fc9c022a
Syed Shibli <syed.m.shibli@et.apyl.com>
2021-09-29 00:34:21 +0530

    User Session Test Case and User Registration Test case

--- spec/requests/spree/user_registrations_controller_spec.rb
+++ spec/requests/spree/user_registrations_controller_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe Spree::UserRegistrationsController, type: :controller do
   let(:admin_user) { create(:spree_user,:with_super_admin_role,password: 'opspi@123') }
   let(:admin_store) {create(:spree_store,url: 'exapmple.com')}
+
+  let(:store_admin) { create(:spree_user,:with_store_admin_role,password: 'opspi@123') }
+  let(:reseller_store){create(:spree_store,url: 'reseller1.example.com',admin_email: store_admin.email)}
   
 
   before(:each) { @routes = Spree::Core::Engine.routes }
@@ -17,11 +20,12 @@ RSpec.describe Spree::UserRegistrationsController, type: :controller do
       context "Valid Registration details" do
         context "and html format is used" do
           it "redirect if success" do
-            store_admin_email = "reseller1@exapmple.com"#Faker::Internet.unique.email 
+            store_admin_email = "reseller1@exapmple.com"# 
            
-            post :create, params: { spree_user: { business_name: "store01", subdomain: "store01",email: store_admin_email,password: "opspi@123",password_confirmation: "opspi@123",reseller_signup: true }}
+            post :create, params: { spree_user: { business_name: "store01", subdomain: "store01",email: store_admin_email,password: "opspi@123",password_confirmation: "opspi@123",
+              reseller_signup: true }}
             
-            
+           
             #Signed up flash message should be displayed on successfully signing up
             expect(flash[:notice]).to eq(I18n.t('devise.user_registrations.signed_up'))
             
@@ -36,13 +40,10 @@ RSpec.describe Spree::UserRegistrationsController, type: :controller do
 
             #expect a new Spree Store is created
 
-
           end
-
         end
       end
     end
-
 
 
      context "Reseller store [reseller1.exapmple.com]" do
@@ -51,6 +52,25 @@ RSpec.describe Spree::UserRegistrationsController, type: :controller do
         @request.host = reseller_store.url #reseller1.exapmple.com.me
       end
 
-     end
+       context "Valid Registration details" do
+        context "and html format is used" do
+          it "redirect if success" do
+            customer_email = Faker::Internet.unique.email
 
+            post :create, params: { spree_user: {email: customer_email,password: "opspi@123",password_confirmation: "opspi@123" }}
+
+             #Signed up flash message should be displayed on successfully signing up
+            expect(flash[:notice]).to eq(I18n.t('devise.user_registrations.signed_up'))
+
+            customer = Spree::User.find_by_email(customer_email)
+
+            #New user on admin store must have admin tenat id
+            expect(customer.account_id).to eq(reseller_store.account_id)
+
+          end
+
+        end
+      end
+
+  end
 end
--- spec/requests/spree/user_sessions_controller_spec.rb
+++ spec/requests/spree/user_sessions_controller_spec.rb
@@ -7,72 +7,59 @@ RSpec.describe Spree::UserSessionsController, type: :controller do
   let(:store_admin) { create(:spree_user,:with_store_admin_role,password: 'opspi@123') }
   let(:store_admin2) { create(:spree_user,:with_store_admin_role,password: 'opspi@123') }
 
-  let(:reseller_store){create(:spree_store,url: 'reseller1.exapmple.com.me',admin_email: store_admin.email)}
-  let(:reseller_store2){create(:spree_store,url: 'reseller2.exapmple.com.me',admin_email: store_admin2.email)}
+  let(:reseller_store){create(:spree_store,url: 'reseller1.example.com',admin_email: store_admin.email)}
+  let(:reseller_store2){create(:spree_store,url: 'reseller2.example.com',admin_email: store_admin2.email)}
 
   before(:each) { @routes = Spree::Core::Engine.routes }
   before { @request.env['devise.mapping'] = Devise.mappings[:spree_user] }
   
   
-    context "store [exapmple.com.me]" do
-      before(:each) do
-        @request.host = admin_store.url #exapmple.com.me
-      end
-
-      context "using incorrect login information on admin store" do
-        context "and html format is used" do
-          it "renders new template again with errors" do
-            post :create, params: { spree_user: { email: admin_user.email, password: 'wrongPass123' }}
-            expect(flash[:error]).to eq(I18n.t('devise.failure.invalid'))
-          end
-        end
-      end
-
-      context '#create session for super admin' do
-        it 'using correct login information on admin store' do
-          post :create, params: { spree_user: { email: admin_user.email ,password: 'opspi@123'} }
-          expect(flash[:error]).to eq(nil)
-          expect(flash[:success]).to eq("Logged in successfully")
-        end
-      end
-
-      context '#create session for store_admin' do
-        it 'using correct login information on admin store' do
-          post :create, params: { spree_user: { email: store_admin.email ,password: 'opspi@123'} }
-          
-          expect(flash[:error]).to eq(nil)
-          expect(flash[:success]).to eq("Logged in successfully")
-        end
-      end
-
+  context "store [example.com]" do
+    before(:each) do
+      @request.host = admin_store.url #example.com
     end
 
-     context "store [reseller1.exapmple.com.me]" do
-
-      before(:each) do
-        @request.host = reseller_store.url #reseller1.exapmple.com.me
-      end
-
-       context "#create" do
-        context "and html format is used" do
-          it "store admin of another store can not login to different store" do
-            post :create, params: { spree_user: { email: store_admin2.email, password: 'opspi@123' }}
-            expect(flash[:alert]).to eq("Invalid Username or Password") # check userDecorator devise active for authentication
-          end
-
-          # it "store admin can login to thier store only" do
-          #   post :create, params: { spree_user: { email: store_admin.email, password: 'opspi@123' }}
-          #   byebug
-          #   expect(flash[:error]).to eq(nil) # check userDecorator devise active for authentication
-          #   expect(flash[:success]).to eq("Logged in successfully")
-          # end
-          
+    context "using incorrect login information on admin store" do
+      context "and html format is used" do
+        it "renders new template again with errors" do
+          post :create, params: { spree_user: { email: admin_user.email, password: 'wrongPass123' }}
+          expect(flash[:error]).to eq(I18n.t('devise.failure.invalid'))
         end
       end
+    end
 
+    context '#create session for super admin' do
+      it 'using correct login information on admin store' do
+        post :create, params: { spree_user: { email: admin_user.email ,password: 'opspi@123'} }
+        expect(flash[:error]).to eq(nil)
+        expect(flash[:success]).to eq("Logged in successfully")
+      end
+    end
 
+    context '#create session for store_admin' do
+      it 'using correct login information on admin store' do
+        post :create, params: { spree_user: { email: store_admin.email ,password: 'opspi@123'} }
+        
+        expect(flash[:error]).to eq(nil)
+        expect(flash[:success]).to eq("Logged in successfully")
+      end
+    end
 
+  end
 
-     end
+  context "store [reseller1.example.com]" do
+    before(:each) do
+      @request.host = reseller_store.url #reseller1.example.com
+    end
 
+    context "#create" do
+      context "and html format is used" do
+        it "store admin of another store can not login to different store" do
+          post :create, params: { spree_user: { email: store_admin2.email, password: 'opspi@123' }}
+          expect(flash[:alert]).to eq("Invalid Username or Password") # check userDecorator devise active for authentication
+        end          
+      end
+    end
+  end
+  
 end
