8dd3e1ed81e6c9c75575a6c7a1af4fdb6e0c5427
Syed Shibli <syed.m.shibli@et.apyl.com>
2021-10-05 09:16:14 +0530

    Indentation

--- app/models/concerns/redis_concern.rb
+++ app/models/concerns/redis_concern.rb
@@ -1,22 +1,23 @@
 module RedisConcern
   extend ActiveSupport::Concern
 
-  	def get_password(panel='solid_cp')
-	   	Sidekiq.redis{|conn|conn.get("spree_user_id_#{user.id}_#{panel}")}
-	  end
+  def get_password(panel='solid_cp')
+    Sidekiq.redis{|conn|conn.get("spree_user_id_#{user.id}_#{panel}")}
+  end
 
-   	def set_password(panel='solid_cp',password=nil)
-   		password = SolidCp::Misc.password_generator if password.blank?
-   		Sidekiq.redis{|conn|conn.set("spree_user_id_#{user.id}_#{panel}", password)}
-   	end
+  def set_password(panel='solid_cp',password=nil)
+    password = SolidCp::Misc.password_generator if password.blank?
+    Sidekiq.redis{|conn|conn.set("spree_user_id_#{user.id}_#{panel}", password)}
+  end
 
-   	def get_username
-   		return user.isp_config_username if user.isp_config_username.present?
-    		"#{user.email[/^[^@]+/]}_#{user.id}"
-  	end
+  def get_username
+    return user.isp_config_username if user.isp_config_username.present?
+    
+    "#{user.email[/^[^@]+/]}_#{user.id}"
+  end
 
-   	def remove_passoword_key(panel='solid_cp')
-   		Sidekiq.redis{|conn|conn.del("spree_user_id_#{user.id}_#{panel}")}
-   	end
+  def remove_passoword_key(panel='solid_cp')
+    Sidekiq.redis{|conn|conn.del("spree_user_id_#{user.id}_#{panel}")}
+  end
     
 end
