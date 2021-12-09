c8357ca17bf0d7923badff0d57314ee8f74d9d25
Bhuvaneshwari <Bhuvana@Bhuvanas-MacBook-Air.local>
2021-12-09 11:54:29 +0530

    New done button in resource edit page

--- app/controllers/spree/admin/users_controller_decorator.rb
+++ app/controllers/spree/admin/users_controller_decorator.rb
@@ -8,6 +8,21 @@ module Spree
 	    		end
 	    		@users = @collection
 	    	end	
+
+			## Todo: Need to customize in callback way(here we dont have any callback)
+			def update
+				if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+				  params[:user].delete(:password)
+				  params[:user].delete(:password_confirmation)
+				end
+		
+				if @user.update(user_params)
+				  flash[:success] = Spree.t(:account_updated)
+				  redirect_to params.key?(:done) ? admin_users_path : edit_admin_user_path(@user)
+				else
+				  render :edit, status: :unprocessable_entity
+				end
+			end
 	    end
 	 end
 end
