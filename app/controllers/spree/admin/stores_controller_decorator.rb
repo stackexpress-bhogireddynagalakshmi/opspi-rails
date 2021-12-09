d1d271649540eb5e9a98b789b58306ac4464ce6e
Bhuvaneshwari <Bhuvana@Bhuvanas-MacBook-Air.local>
2021-12-09 14:05:57 +0530

    indent

--- app/controllers/spree/admin/stores_controller_decorator.rb
+++ app/controllers/spree/admin/stores_controller_decorator.rb
@@ -1,6 +1,6 @@
 module Spree
 	module Admin
-	    module StoresControllerDecorator
+	  module StoresControllerDecorator
       ## Todo: Need to customize in callback way(here we dont have any callback)
 			def update
 				@store.assign_attributes(permitted_store_params)
@@ -12,9 +12,9 @@ module Spree
 				  flash[:error] = "#{Spree.t('store_errors.unable_to_update')}: #{@store.errors.full_messages.join(', ')}"
 				  redirect_to spree.edit_admin_store_path(@store)
 				end
-			  end
-	    end
-	 end
+			end
+	  end
+	end
 end
 
 ::Spree::Admin::StoresController.prepend Spree::Admin::StoresControllerDecorator if ::Spree::Admin::StoresController.included_modules.exclude?(Spree::Admin::StoresControllerDecorator)
