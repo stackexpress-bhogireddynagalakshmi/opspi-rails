6cb4414e2cbffc22f3b668ab1fe79eab40145aef
Syed Shibli <syed.m.shibli@et.apyl.com>
2021-12-25 11:56:15 +0530

    Read Timeout SOAP fixes

--- app/lib/solid_cp/package.rb
+++ app/lib/solid_cp/package.rb
@@ -1,56 +1,57 @@
 module SolidCp
 
-	class Package < Base
+  class Package < Base
 
-		attr_reader :user
+    attr_reader :user
 
-		client wsdl: SOAP_PLAN_WSDL, endpoint: SOAP_PLAN_WSDL,log: SolidCp::Config.log
-		global :open_timeout, SolidCp::Config.timeout
-  		global :basic_auth, SolidCp::Config.username, SolidCp::Config.password
+    client wsdl: SOAP_PLAN_WSDL, endpoint: SOAP_PLAN_WSDL,log: SolidCp::Config.log
+    global :read_timeout, SolidCp::Config.timeout
+    global :open_timeout, SolidCp::Config.timeout
+    global :basic_auth, SolidCp::Config.username, SolidCp::Config.password
 
-		
-		operations	:get_packages,:get_nested_packages_summary,:get_raw_packages,:get_packages_paged,:get_my_packages,
-  		    :get_raw_my_packages, :get_package, :get_package_context, :get_package_quotas,:get_package_quotas_for_edit, 
-  		    :add_package, :update_package, :update_package_literal, :update_package_name,
-  			:delete_package, :change_package_status, :evaluate_user_package_tempate, :get_package_settings, :update_package_settings,
-  			:set_default_top_package, :get_package_addons, :get_package_addon, :add_package_addon_by_id, :add_package_addon,
-  			:add_package_addon_literal, :update_package_addon, :update_package_addon_literal, :delete_package_addon,
-  			:get_raw_package_items_by_type, :get_raw_package_items_paged, :get_raw_package_items, 
-  			:detach_package_item, :move_package_item, :get_package_quota,:send_package_summary_letter,:get_evaluated_package_template_body,
-  			:add_package_with_resources, :create_user_wizard,:get_package_packages,:get_nested_packages_paged,
-  			:get_packages_bandwidth_paged, :get_packages_diskspace_paged, :get_package_bandwidth, :get_package_diskspace
-  		
+    
+    operations  :get_packages,:get_nested_packages_summary,:get_raw_packages,:get_packages_paged,:get_my_packages,
+          :get_raw_my_packages, :get_package, :get_package_context, :get_package_quotas,:get_package_quotas_for_edit, 
+          :add_package, :update_package, :update_package_literal, :update_package_name,
+        :delete_package, :change_package_status, :evaluate_user_package_tempate, :get_package_settings, :update_package_settings,
+        :set_default_top_package, :get_package_addons, :get_package_addon, :add_package_addon_by_id, :add_package_addon,
+        :add_package_addon_literal, :update_package_addon, :update_package_addon_literal, :delete_package_addon,
+        :get_raw_package_items_by_type, :get_raw_package_items_paged, :get_raw_package_items, 
+        :detach_package_item, :move_package_item, :get_package_quota,:send_package_summary_letter,:get_evaluated_package_template_body,
+        :add_package_with_resources, :create_user_wizard,:get_package_packages,:get_nested_packages_paged,
+        :get_packages_bandwidth_paged, :get_packages_diskspace_paged, :get_package_bandwidth, :get_package_diskspace
+      
 
-	    def initialize user
-	       @user = user
-	    end
+      def initialize user
+         @user = user
+      end
 
 
-	    # Creates Hosting Space on SolidCP for a user
-	    
-	    def  add_package(plan_id=10)
-	    	if user.packages.where(solid_cp_master_plan_id: plan_id).first.blank?
-	    		org_name = TenantManager::TenantHelper.unscoped_query{user.account.orgainization_name}
-		    	response  = super(message: {
-			    	 user_id: user.solid_cp_id,
-			    	 plan_id: plan_id,
-			    	 package_name:"#{org_name.snakecase}_hosting_space_#{plan_id}",
-			    	 package_comments: "Hosting space for #{user.full_name}",
-			    	 status_id: 1,
-			    	 purchase_date: Time.now.strftime('%Y-%m-%dT%H:%M:%S.%L'),
-		    	})
-		    
-		    	if response.success? && response.body[:add_package_response][:add_package_result][:result].to_i > 0
-		    		user.packages.create({:solid_cp_package_id=>response.body[:add_package_response][:add_package_result][:result],
-		    			solid_cp_master_plan_id: plan_id})
-		    		{:success=>true, :message=>'SolidCP Package created successfully',response: response}
-		    	else
-		    		{:success=>true, :message=>'Something went wrong. Please try again.',response: response}
-		    	end
-		    else
-		    	{:success=>true, :message=>'SolidCP Package already exists'}
-		    end
-	    end
+      # Creates Hosting Space on SolidCP for a user
+      
+      def  add_package(plan_id=10)
+        if user.packages.where(solid_cp_master_plan_id: plan_id).first.blank?
+          org_name = TenantManager::TenantHelper.unscoped_query{user.account.orgainization_name}
+          response  = super(message: {
+             user_id: user.solid_cp_id,
+             plan_id: plan_id,
+             package_name:"#{org_name.snakecase}_hosting_space_#{plan_id}",
+             package_comments: "Hosting space for #{user.full_name}",
+             status_id: 1,
+             purchase_date: Time.now.strftime('%Y-%m-%dT%H:%M:%S.%L'),
+          })
+        
+          if response.success? && response.body[:add_package_response][:add_package_result][:result].to_i > 0
+            user.packages.create({:solid_cp_package_id=>response.body[:add_package_response][:add_package_result][:result],
+              solid_cp_master_plan_id: plan_id})
+            {:success=>true, :message=>'SolidCP Package created successfully',response: response}
+          else
+            {:success=>true, :message=>'Something went wrong. Please try again.',response: response}
+          end
+        else
+          {:success=>true, :message=>'SolidCP Package already exists'}
+        end
+      end
 
-	end
+  end
 end
--- app/lib/solid_cp/plan.rb
+++ app/lib/solid_cp/plan.rb
@@ -1,150 +1,147 @@
 module SolidCp
 
-	class Plan < Base
+  class Plan < Base
 
-		attr_reader :user,:plan
+    attr_reader :user,:plan
 
-		client wsdl: SOAP_PLAN_WSDL, endpoint: SOAP_PLAN_WSDL,log: SolidCp::Config.log
-		global :open_timeout, SolidCp::Config.timeout
-  		global :basic_auth, SolidCp::Config.username, SolidCp::Config.password
+    client wsdl: SOAP_PLAN_WSDL, endpoint: SOAP_PLAN_WSDL,log: SolidCp::Config.log
+    global :read_timeout, SolidCp::Config.timeout
+    global :open_timeout, SolidCp::Config.timeout
+    global :basic_auth, SolidCp::Config.username, SolidCp::Config.password
 
-  		operations :get_hosting_plans, :get_hosting_addons, :get_hosting_plan, :get_hosting_plan_quotas, 
-  			:get_hosting_plan_context, :get_user_available_hosting_plans, :get_user_available_hosting_addons, 
-  			:add_hosting_plan, :update_hosting_plan, :delete_hosting_plan, :search_service_items_paged, 
-  			:get_search_object,:get_search_object_quick_find,:get_search_table_by_columns, 
-  			:get_searchable_service_item_types, :send_account_summary_letter, :get_evaluated_account_template_body, 
-  			:get_overusage_summary_report, :get_diskspace_overusage_details_report, :get_bandwidth_overusage_details_report
+    operations :get_hosting_plans, :get_hosting_addons, :get_hosting_plan, :get_hosting_plan_quotas, 
+      :get_hosting_plan_context, :get_user_available_hosting_plans, :get_user_available_hosting_addons, 
+      :add_hosting_plan, :update_hosting_plan, :delete_hosting_plan, :search_service_items_paged, 
+      :get_search_object,:get_search_object_quick_find,:get_search_table_by_columns, 
+      :get_searchable_service_item_types, :send_account_summary_letter, :get_evaluated_account_template_body, 
+      :get_overusage_summary_report, :get_diskspace_overusage_details_report, :get_bandwidth_overusage_details_report
 
+      def initialize user,plan
+         @user = user
+         @plan = plan
+      end
 
-  		
-	    def initialize user,plan
-	       @user = user
-	       @plan = plan
-	    end
-
-
-	    def self.get_hosting_plans
-
-	    	response = super(message: { user_id: 1  })
-	    end
+      def self.get_hosting_plans
+        response = super(message: { user_id: 1  })
+      end
 
 
-	    def get_hosting_plans
-	    	response = super(message: { user_id: user.solid_cp_id  })
-	    end
+      def get_hosting_plans
+        response = super(message: { user_id: user.solid_cp_id  })
+      end
 
 
-	    def add_hosting_plan
-	    	# ServerId will be used to create master plan
-	    	if plan.solid_cp_plan_id.blank?
-	    		
-	    		user.solid_cp.package.add_package(plan.solid_cp_master_plan_id)	   	
-	    
-				response = super(message: { 
-			   		plan: {
-			   			"PackageId" => user.packages.first.try(:solid_cp_package_id),
-				   		"PlanName" => plan.name,
-				   		"PlanDescription"=>plan.description,
-				   		"Available"=> true,
-				   		"IsAddon"=> false,
-				   		"SetupPrice"=> 0.0,
-				   		"RecurringPrice" =>0.0000,
-				   		"RecurrenceUnit"=> 2,
-				   		"RecurrenceLength" =>1,
-				   		"Groups" => get_hosting_plan_groups_info,
-				   		"Quotas" => get_hosting_plan_quotas_info,
-				   		"UserId"=>user.solid_cp_id	   		
-			   		}
-			   	 })
-			
-				if response.success? && response.body[:add_hosting_plan_response][:add_hosting_plan_result].to_i > 0 
-					plan.solid_cp_plan_id = response.body[:add_hosting_plan_response][:add_hosting_plan_result].to_i
-					plan.save!
-				else
-					{:success=>false,:message=> "Something went wrong. Please try again.",response: response}
-				end
-			else
-					{:success=>true,:message=> "Plan Already exists.",response: response}
-			end
-	    end
+      def add_hosting_plan
+        # ServerId will be used to create master plan
+        if plan.solid_cp_plan_id.blank?
+          
+          user.solid_cp.package.add_package(plan.solid_cp_master_plan_id)       
+      
+        response = super(message: { 
+             plan: {
+               "PackageId" => user.packages.first.try(:solid_cp_package_id),
+               "PlanName" => plan.name,
+               "PlanDescription"=>plan.description,
+               "Available"=> true,
+               "IsAddon"=> false,
+               "SetupPrice"=> 0.0,
+               "RecurringPrice" =>0.0000,
+               "RecurrenceUnit"=> 2,
+               "RecurrenceLength" =>1,
+               "Groups" => get_hosting_plan_groups_info,
+               "Quotas" => get_hosting_plan_quotas_info,
+               "UserId"=>user.solid_cp_id         
+             }
+            })
+      
+        if response.success? && response.body[:add_hosting_plan_response][:add_hosting_plan_result].to_i > 0 
+          plan.solid_cp_plan_id = response.body[:add_hosting_plan_response][:add_hosting_plan_result].to_i
+          plan.save!
+        else
+          {:success=>false,:message=> "Something went wrong. Please try again.",response: response}
+        end
+      else
+          {:success=>true,:message=> "Plan Already exists.",response: response}
+      end
+      end
 
-	    def update_hosting_plan
-	    	if plan.solid_cp_plan_id.present?
+      def update_hosting_plan
+        if plan.solid_cp_plan_id.present?
 
-	    		response = super(message: { 
-			   		plan: {
-			   			"PlanId" => plan.solid_cp_plan_id,
-			   			"PackageId" => user.packages.first.try(:solid_cp_package_id),
-				   		"PlanName" => plan.name,
-				   		"PlanDescription"=>plan.description,
-				   		"Available"=> true,
-				   		"IsAddon"=> false,
-				   		"SetupPrice"=> 0.0,
-				   		"RecurringPrice" =>0.0000,
-				   		"RecurrenceUnit"=> 2,
-				   		"RecurrenceLength" =>1,
-				   		"Groups" => get_hosting_plan_groups_info,
-				   		"Quotas" => get_hosting_plan_quotas_info,
-				   		"UserId"=>user.solid_cp_id	   		
-			   		}
-			   	 })
-	    	end
-	    end
+          response = super(message: { 
+             plan: {
+               "PlanId" => plan.solid_cp_plan_id,
+               "PackageId" => user.packages.first.try(:solid_cp_package_id),
+               "PlanName" => plan.name,
+               "PlanDescription"=>plan.description,
+               "Available"=> true,
+               "IsAddon"=> false,
+               "SetupPrice"=> 0.0,
+               "RecurringPrice" =>0.0000,
+               "RecurrenceUnit"=> 2,
+               "RecurrenceLength" =>1,
+               "Groups" => get_hosting_plan_groups_info,
+               "Quotas" => get_hosting_plan_quotas_info,
+               "UserId"=>user.solid_cp_id         
+             }
+            })
+        end
+      end
 
 
-	    def get_hosting_plan_groups_info
-	    	group_hash = {}
-	        arr = plan.plan_quota_groups.collect do |quota_group| 
-	    	 	{
-	   				"GroupId" => quota_group.solid_cp_quota_group_id,
-	   				"Enabled" => quota_group.enabled,
-	   				"CalculateDiskSpace" => quota_group.calculate_diskspace,
-	   				"CalculateBandwidth" => quota_group.calculate_bandwidth
-   				}
-	    	end
-	    	group_hash["HostingPlanGroupInfo"] = arr
-	    	group_hash
-	    end
+      def get_hosting_plan_groups_info
+        group_hash = {}
+          arr = plan.plan_quota_groups.collect do |quota_group| 
+           {
+             "GroupId" => quota_group.solid_cp_quota_group_id,
+             "Enabled" => quota_group.enabled,
+             "CalculateDiskSpace" => quota_group.calculate_diskspace,
+             "CalculateBandwidth" => quota_group.calculate_bandwidth
+           }
+        end
+        group_hash["HostingPlanGroupInfo"] = arr
+        group_hash
+      end
 
-	    def get_hosting_plan_quotas_info
-	    	quota_hash = {}
-	    	arr = plan.plan_quotas.uniq(&:solid_cp_quota_id).collect do |plan_quota|
-		    	{
-		    		"QuotaId" => plan_quota.solid_cp_quota_id,
-		    		"QuotaValue" => plan_quota.quota_value
-		    	}
-		    	
-	    	end
-	    	quota_hash["HostingPlanQuotaInfo"] = arr
-	    	quota_hash
-	    end
+      def get_hosting_plan_quotas_info
+        quota_hash = {}
+        arr = plan.plan_quotas.uniq(&:solid_cp_quota_id).collect do |plan_quota|
+          {
+            "QuotaId" => plan_quota.solid_cp_quota_id,
+            "QuotaValue" => plan_quota.quota_value
+          }
+          
+        end
+        quota_hash["HostingPlanQuotaInfo"] = arr
+        quota_hash
+      end
 
-	   
+     
 
-	    def self.get_hosting_plan_quotas(plan_id)
-	    	response = super(message: { plan_id: plan_id }) rescue []
-	    end
+      def self.get_hosting_plan_quotas(plan_id)
+        response = super(message: { plan_id: plan_id }) rescue []
+      end
 
-	     #syncronous api call
-	    def get_hosting_plan_quotas
-	    	response = super(message: { plan_id: plan.solid_cp_plan_id })
-	    end
+       #syncronous api call
+      def get_hosting_plan_quotas
+        response = super(message: { plan_id: plan.solid_cp_plan_id })
+      end
 
-	    #syncronous api call
-	    def self.master_plans_dropdown
+      #syncronous api call
+      def self.master_plans_dropdown
 
-	    	begin
-	    		response = SolidCp::Plan.get_hosting_plans 
-					plans  = response.body[:get_hosting_plans_response][:get_hosting_plans_result][:diffgram][:new_data_set][:table]	
-	    	
-		    	plans = plans.collect{|x| [x[:plan_name],x[:plan_id]]}
-		    	plans
+        begin
+          response = SolidCp::Plan.get_hosting_plans 
+          plans  = response.body[:get_hosting_plans_response][:get_hosting_plans_result][:diffgram][:new_data_set][:table]  
+        
+          plans = plans.collect{|x| [x[:plan_name],x[:plan_id]]}
+          plans
 
-	    	rescue Exception => e
-	    		Rails.logger.error {e.message}
-	    		plans = [] 
-	    	end	
-	    end
+        rescue Exception => e
+          Rails.logger.error {e.message}
+          plans = [] 
+        end  
+      end
 
-	end
+  end
 end
