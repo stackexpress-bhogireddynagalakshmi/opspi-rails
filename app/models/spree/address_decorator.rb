a8dd13d05f4266497d3b9b05d41083a69b7ee927
Syed Shibli <syed.m.shibli@et.apyl.com>
2021-12-27 15:11:33 +0530

    Country Code

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -1,24 +1,30 @@
 module Spree
 	module AddressDecorator
-    Spree::Address::ADDRESS_FIELDS = %w(firstname lastname company country_code phone address1 address2 country state city zipcode).freeze
+    Spree::Address::ADDRESS_FIELDS = %w(firstname lastname company phone address1 address2 country state city zipcode).freeze
 
     def self.prepended(base)
       base.after_commit :set_first_and_last_name, on: [:update]
+      base.after_commit :set_country_code, on: [:update, :create]
     end
 
     def set_first_and_last_name
-      #return unless user_id_changed?
       return unless self.user.present?
       return if self.user.first_name.present? && self.user.last_name.present?
 
-       self.user.first_name =  self.firstname
-       self.user.last_name  =  self.lastname
-       self.user.save
+      self.user.first_name =  self.firstname
+      self.user.last_name  =  self.lastname
+      self.user.save
+    end
+
+    def set_country_code
+      return unless country.present?
+
+      code = IsoCountryCodes.find(country.iso)
+      update_column(:country_code,code.calling)
     end
    
 	end
 end
 
 ::Spree::Address.prepend Spree::AddressDecorator if ::Spree::Address.included_modules.exclude?(Spree::AddressDecorator)
-Spree::PermittedAttributes.address_attributes << :country_code
 
