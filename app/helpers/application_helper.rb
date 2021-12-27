aa545751d74073e3085805289485a5af352b58d4
Bhuvaneshwari <Bhuvana@Bhuvanas-MacBook-Air.local>
2021-12-27 20:24:21 +0530

    country code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -38,34 +38,6 @@ module ApplicationHelper
     date.strftime("%m/%d/%y")
   end
 
-  ### Need to add all the country codes ###
-  def country_codes
-    @countries = { "+93  Afghanistan" => "+93", "+355  Albania	" => "+355", "+213  Algeria	" => "+213", "+1 684 American Samoa" => "+1 684",
-                   "+376 Andorra" => "+376", "+244 Angola" => "+244", "+1 264 Anguilla" => "+1 264", "+1 268 Antigua and Barbuda" => "+1 268",
-                   "+54 Argentina" => "+54", "+374 Armenia" => "+374", "+297 Aruba" => "+297", "+247 Ascension" => "+247",
-                   "+61 Australia" => "+61", "+672 1 Australian Antarctic Territory" => "+672 1", "+672 Australian External Territories" => "+672", "+43 Austria" => "+43",
-                   "+994 Azerbaijan" => "+994", "+1 242 Bahamas" => "+1 242", "+973 Bahrain" => "+973", "+880 Bangladesh" => "+880",
-                   "+1 246 Barbados" => "+1 246", "+1 268 Barbuda" => "+1 268", "+375 Belarus" => "+375", "+32 Belgium" => "+32",
-                   "+501 Belize" => "+501", "+229 Benin" => "+229", "+1 441 Bermuda" => "+1 441", "+975 Bhutan" => "+975",
-                   "+591 Bolivia" => "+591","+267 Botswana" => "+267", "+55 Brazil" => "+55","+673 Brunei" => "+673","+673 Bulgaria" => "+673", 
-                   "+359 Burkina Faso " => "+359","+226 Burundi" => "+226",
-                   "+855 Cambodia" => "+855","+237 Cameroon" => "+237","+238 Cape Verde" => "+238",
-                   "+1 345 Cayman Islands" => "+1 345","+236 Central African Republic" => "+236","+235 Chad" => "+235","+56 Chile" => "+56",
-                   "+86 China" => "+86","+61 Christmas Island" => "+61","+61 Cocos (Keeling) Islands" => "+61","+57 Colombia" => "+57",
-                   "+269 Comoros" => "+269","+243 Congo(Brazzaville)" => "+243","+242 Congo(Kinshasa)" => "+242","+682 Cook Islands" => "+682",
-                   "+506 Costa Rica" => "+506","+225 Cote D Ivoire" => "+225","+385 Croatia" => "+385","+53 Cuba" => "+53",
-                   "+357 Cyprus" => "+357","+420 Czech Republic" => "+420","+45 Denmark" => "+45","+253 Djibouti " => "+253",
-                   "+1 767 Dominica" => "+1 767","+1 809 Dominican Republic" => "+1 809","+358 Finland" => "+358","+33 France" => "+33",
-                   "+995 Georgia" => "+995","+49 Germany" => "+49","+ 30 Greece" => "+ 30","+299 Greenland" => "+299",
-                   "+852 Hong Kong" => "+852","+36 Hungary" => "+36","+354 Iceland" => "+354","+91 India" => "+91","+62 Indonesia" => "+62",
-                   "+98 Iran" => "+98","+964 Iraq" => "+964","+353 Ireland" => "+353","+39 Italy" => "+39",
-                   "+81 Japan" => "+81","+850 Korea, North" => "+850","+82 Korea, South" => "+82","+60 Malaysia" => "+60",
-                   "+960 Maldives" => "+960","+230 Mauritius" => "+230","+52 Mexico" => "+52","+977 Nepal" => "+977",
-                   "+47 Norway" => "+47","+968 Oman" => "+968","+92 Pakistan" => "+92","+974 Qatar" => "+974",
-                   "+7 Russia" => "+7","+966 Saudi Arabia" => "+966","+65 Singapore" => "+65","+44 United Kingdom" => "+44","+1 United States" => "+1"}
-  end
-
-
   def current_available_payment_methods(user)
     if user.present? && user.store_admin?
       TenantManager::TenantHelper.unscoped_query{TenantManager::TenantHelper.admin_tenant.payment_methods.available_on_front_end.select { |pm| pm.available_for_order?(self) }}
--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -1,6 +1,6 @@
 module Spree
 	module AddressDecorator
-    Spree::Address::ADDRESS_FIELDS = %w(firstname lastname company phone address1 address2 country state city zipcode).freeze
+    Spree::Address::ADDRESS_FIELDS = %w(firstname lastname company country phone address1 address2  state city zipcode).freeze
 
     def self.prepended(base)
       base.after_commit :set_first_and_last_name, on: [:update]
