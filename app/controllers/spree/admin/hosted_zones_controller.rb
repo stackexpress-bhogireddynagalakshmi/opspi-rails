09c4870dcbc580950c2ebb02a67b59670d4a99cf
Bhuvana101193 <bhuvaneshwari.uyyalam@apyl.com>
2022-02-01 13:46:18 +0530

    fixed redirection issue

--- app/controllers/spree/admin/hosted_zones_controller.rb
+++ app/controllers/spree/admin/hosted_zones_controller.rb
@@ -13,7 +13,11 @@ class Spree::Admin::HostedZonesController < Spree::Admin::BaseController
     def create
       @response = IspConfig::HostedZone.new(host_zone_params).create
       set_flash
-      redirect_to admin_hosted_zones_path
+      if @response[:success]
+        redirect_to admin_hosted_zones_path
+      else
+        render :new
+      end
     end
 
     def new; end
@@ -26,7 +30,13 @@ class Spree::Admin::HostedZonesController < Spree::Admin::BaseController
     def update
       @response  = IspConfig::HostedZone.new(host_zone_params).update(@zone_list.isp_config_host_zone_id)
       set_flash
-      redirect_to admin_hosted_zones_path
+      if @response[:success]
+        redirect_to admin_hosted_zones_path
+      else
+        response = IspConfig::HostedZone.new(@zone_list).get_zone
+        @hosted_zone = response[:response].response  if response[:success].present?
+        render :edit
+      end
     end
 
     def destroy
@@ -57,7 +67,7 @@ class Spree::Admin::HostedZonesController < Spree::Admin::BaseController
     end
 
     def validate_params(dns_data)
-        return {success: false,msg: I18n.t('isp_config.required_field_missing')} if dns_data[:name].blank? || dns_data[:status].blank?
+        return {success: false,msg: I18n.t('isp_config.required_field_missing')} if dns_data[:name].blank? || dns_data[:ns].blank? || dns_data[:mbox].blank?
 
         re = /(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]/
         return  dns_data[:name].match?(re) ?  {success: true} :  {success: false,msg: I18n.t('isp_config.invalid_zone_name')}
@@ -65,7 +75,7 @@ class Spree::Admin::HostedZonesController < Spree::Admin::BaseController
     end
 
     def host_zone_params
-      params.require(:hosted_zones).permit(:name,:ns,:mbox,:refresh,:retry,:expire,:minimum,:ttl,:xfer,:also_notify,:update_acl,:isp_config_host_zone_id,:status,:hosted_zones,:commit).merge!({isp_config_id: current_spree_user.isp_config_id})
+      params.require(:hosted_zones).permit(:name,:ns,:mbox,:refresh,:retry,:expire,:minimum,:ttl,:xfer,:also_notify,:update_acl,:isp_config_host_zone_id,:status).merge!({isp_config_id: current_spree_user.isp_config_id})
     end
 
 end
