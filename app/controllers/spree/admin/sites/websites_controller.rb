e201d8453667c00f2e31d388ee2003b568043df4
Rakesh Bakoriya <rakesh.bakoriya@apyl.com>
2022-02-08 11:14:37 +0530

    Add reusability code

--- app/controllers/spree/admin/sites/websites_controller.rb
+++ app/controllers/spree/admin/sites/websites_controller.rb
@@ -1,67 +1,84 @@
 module Spree
   module Admin
      module Sites
-      class WebsitesController < Spree::Admin::BaseController
-        before_action :set_website, only: [:destroy, :update]
-
-        def index
-          response = website_api.all || []
-          if response[:success]
-            @websites  = response[:response].response
-          else
-            @websites = []
-          end
-        end
-
-        def new; end
-
-        def create
-          @response  = website_api.create(website_params)
-          if @response[:success]
-            set_flash
-            redirect_to admin_sites_websites_path
-          else
-            render :new
-          end
-        end
-
-        def destroy
-          @response  = website_api.destroy(@website.isp_config_website_id)
-          set_flash
-          redirect_to admin_sites_websites_path
-        end
-
-        def update
-          @response  = website_api.update(@website.isp_config_website_id, website_params)
-          set_flash
-          redirect_to admin_sites_websites_path
-        end
+      class WebsitesController < Spree::Admin::IspConfigResourcesController
 
         private
-
-        def set_flash
-          if @response[:success] 
-            flash[:success] = @response[:message]
-          else
-            flash[:error] = @response[:message] 
-          end
+        def resource_id_field
+          "isp_config_website_id"
         end
 
-        def website_params
+        def assoc
+          "websites"
+        end
+
+        def resource_params
+          request_params.merge(extra_isp_params)
+        end
+
+        def request_params
           params.require("website").permit(:ip_address, :ipv6_address, :domain, :subdomain, :hd_quota, :traffic_quota, :active, :php)
         end
 
-        def website_api
+        def resource_index_path
+          redirect_to admin_sites_websites_path
+        end
+
+        def isp_config_api
           current_spree_user.isp_config.website
         end
 
-        def set_website
-          @website = current_spree_user.websites.find_by_isp_config_website_id(params[:id])
-
-          redirect_to admin_sites_websites_path, notice: 'Not Authorized' if @website.blank?
+        def extra_isp_params
+          {
+            ip_address: '*',
+            type: 'vhost',
+            parent_domain_id: 0,
+            vhost_type: 'name',
+            hd_quota: -1,
+            traffic_quota: -1,
+            cgi: 'y',
+            ssi: 'y',
+            suexec: 'y',
+            errordocs: 1,
+            is_subdomainwww: 1,
+            subdomain: 'www',
+            php: 'y',
+            ruby: 'n',
+            # redirect_type: '',
+            # redirect_path: '',
+            ssl: 'n',
+            # ssl_state: '',
+            # ssl_locality: '',
+            # ssl_organisation: '',
+            # ssl_organisation_unit: '',
+            # ssl_country: '',
+            # ssl_domain: '',
+            # ssl_request: '',
+            # ssl_key: '',
+            # ssl_cert: '',
+            # ssl_bundle: '',
+            # ssl_action: '',
+            # stats_password: '',
+            stats_type: 'webalizer',
+            allow_override: 'All',
+            # apache_directives: '',
+            php_open_basedir: '/',
+            pm: 'ondemand',
+            pm_max_requests: 0,
+            pm_process_idle_timeout: 10,
+            # custom_php_ini: '',
+            # backup_interval: '',
+            backup_copies: 1,
+            backup_format_web: 'default',
+            backup_format_db: 'gzip',
+            # active: 'y',
+            traffic_quota_lock: 'n',
+            http_port: '80',
+            https_port: '443'
+          }
         end
 
       end
-     end
+    end
   end
 end
