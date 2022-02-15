e1f5080dca1c0ebfd891390c5914fbafa4427da7
Syed Shibli <syed.m.shibli@et.apyl.com>
2022-02-15 05:01:56 +0000

    Mail forwarding api integration

---
+++ app/lib/isp_config/mail/mail_forward.rb
@@ -0,0 +1,105 @@
+module IspConfig
+  module Mail
+    class MailForward < Base
+    attr_accessor :user
+
+    def initialize user
+      @user = user
+    end
+
+    def find(id)
+      response = query({
+        :endpoint => '/json.php?mail_forward_get',
+        :method => :GET,
+        :body => { 
+          primary_id: id
+        }}
+      )
+
+      formatted_response(response,'find')
+    end
+
+    def create(params={})
+      response = query({
+        :endpoint => '/json.php?mail_forward_add',
+        :method => :POST,
+        :body => { 
+          client_id: user.isp_config_id,
+          params: params.merge(server_params)
+        }}
+      )
+      user.mail_forwards.create({isp_config_mail_forward_id: response["response"]}) if response.code == "ok"
+      
+      formatted_response(response,'create')
+    end
+
+    def update(primary_id,params={})
+      response = query({
+        :endpoint => '/json.php?mail_forward_update',
+        :method => :POST,
+        :body => { 
+          client_id: user.isp_config_id,
+          primary_id: primary_id,
+          params: params.merge(server_params)
+        }}
+      )
+
+      formatted_response(response,'update')      
+    end
+
+    def destroy(primary_id)
+      response = query({
+        :endpoint => '/json.php?mail_forward_delete',
+        :method => :DELETE,
+        :body => { 
+          client_id: user.isp_config_id,
+          primary_id: primary_id
+        }}
+      )
+      user.mail_forwards.find_by_isp_config_mail_forward_id(primary_id).destroy if response.code == "ok"
+      formatted_response(response,'delete')
+    end
+
+    def all
+      response = query({
+        :endpoint => '/json.php?mail_forward_get',
+        :method => :GET,
+        :body => { 
+          primary_id: "-1"
+        }}
+      )
+      response.response.reject!{|x| mail_forward_ids.exclude?(x.forwarding_id.to_i)}
+
+      formatted_response(response,'list')
+    end
+
+    private
+
+    def mail_forward_ids
+      user.mail_forwards.pluck(:isp_config_mail_forward_id)
+    end
+
+    def formatted_response(response,action)
+      if  response.code == "ok"
+        {
+          :success=>true,
+          :message=>I18n.t("isp_config.mail_forward.#{action}"),
+          response: response
+        }
+      else
+        { 
+          :success=>false,
+          :message=> I18n.t('isp_config.something_went_wrong',message: response.message),
+          response: response
+        }
+      end
+    end
+
+    def server_params
+      {
+        server_id: ENV['ISP_CONFIG_WEB_SERVER_ID']
+      }
+    end
+  end
+  end
+end
