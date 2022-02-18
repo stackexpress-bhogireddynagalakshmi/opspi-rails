d0f24e66dd7a2193d12ab74e11117b9efebf45a5
Rakesh Bakoriya <rakesh.bakoriya@apyl.com>
2022-02-18 21:34:29 +0530

    Fix for subdomain

--- app/controllers/spree/admin/sites/sub_domains_controller.rb
+++ app/controllers/spree/admin/sites/sub_domains_controller.rb
@@ -18,7 +18,11 @@ module Spree
         end
 
         def request_params
-          params.require("sub_domain").permit(:domain ,:parent_domain_id)
+          params.require("sub_domain").permit(:parent_domain_id).merge(domain: convert_sub_domain)
+        end
+
+        def convert_sub_domain
+          params[:parent_domain_name].present? ? (params[:sub_domain][:domain] + "." + params[:parent_domain_name]) : params[:sub_domain][:domain] 
         end
 
         def resource_index_path
@@ -31,59 +35,36 @@ module Spree
 
         def get_websites
           response = current_spree_user.isp_config.website.all || []
-          if response[:success]
-            @websites  = response[:response].response
-          else
-            @websites = []
-          end
+          @websites = response[:success] ? response[:response].response : []
         end
 
         def extra_isp_params
-          {
-            # server_id: 1,
-            ip_address: '*',
-            # domain: 'tsssssubt.int',
-            type: 'subdomain',
-            # parent_domain_id: 1,
-            vhost_type: '',
-            document_root: '/web/dom',
-            system_user: 'benutzer',
-            system_group: 'gruppe',
+          { 
+            server_id: 1,
+            # domain: "k.syednew1.com",
+            type: "subdomain",
+            # parent_domain_id: 51,
+            document_root: "/web/dom",
+            system_user: "benutzer",
+            system_group: "gruppe",
             hd_quota: 100000,
             traffic_quota: -1,
-            cgi: 'y',
-            ssi: 'y',
-            suexec: 'y',
+            cgi: "y",
+            ssi: "y",
+            suexec: "y",
             errordocs: 1,
             is_subdomainwww: 1,
-            subdomain: '',
-            php: 'y',
-            ruby: 'n',
-            redirect_type: '',
-            redirect_path: '',
-            ssl: 'n',
-            ssl_state: '',
-            ssl_locality: '',
-            ssl_organisation: '',
-            ssl_organisation_unit: '',
-            ssl_country: '',
-            ssl_domain: '',
-            ssl_request: '',
-            ssl_cert: '',
-            ssl_bundle: '',
-            ssl_action: '',
-            stats_password: '',
-            stats_type: 'webalizer',
-            allow_override: 'All',
-            apache_directives: '',
-            php_open_basedir: '/php',
-            custom_php_ini: '',
-            backup_interval: '',
+            subdomain: "1",
+            php: "y",
+            ruby: "n",
+            ssl: "n",
+            stats_type: "webalizer",
+            allow_override: "All",
+            apache_directives: "",
+            php_open_basedir: "/php",
             backup_copies: 1,
-            active: 'y',
-            traffic_quota_lock: 'n'
-
-
+            active: "y",
+            traffic_quota_lock: "n"
           }
         end
 
