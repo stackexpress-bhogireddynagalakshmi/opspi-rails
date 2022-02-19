de995ae6838729fe0fa19155e2fd6256e5c0eaeb
Syed Shibli <syed.m.shibli@et.apyl.com>
2022-02-19 11:53:55 +0530

    chanegs

--- app/models/spree/gateway/pay_pal_express_decorator.rb
+++ app/models/spree/gateway/pay_pal_express_decorator.rb
@@ -1,18 +1,16 @@
 # frozen_string_literal: true
 
 module Spree
-  module Gateway
-    module PayPalExpressDecorator
-      def provider
-        ::PayPal::SDK.configure(
-          mode: preferred_server.present? ? preferred_server : "sandbox",
-          username: preferred_login,
-          password: preferred_password,
-          signature: preferred_signature,
-          ssl_options: { ca_file: nil }
-        )
-        provider_class.new
-      end
+  module Gateway::PayPalExpressDecorator
+    def provider
+      ::PayPal::SDK.configure(
+        mode: preferred_server.present? ? preferred_server : "sandbox",
+        username: preferred_login,
+        password: preferred_password,
+        signature: preferred_signature,
+        ssl_options: { ca_file: nil }
+      )
+      provider_class.new
     end
   end
 end
