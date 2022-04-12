9535864b38e7b6f9861c36af73cce280ab633c21
Syed Shibli <syed.m.shibli@et.apyl.com>
2022-04-12 12:53:19 +0530

    Create account on Both panel

--- app/jobs/solid_cp_provisioning_job.rb
+++ app/jobs/solid_cp_provisioning_job.rb
@@ -1,63 +1,69 @@
+# frozen_string_literal: true
+
 class SolidCpProvisioningJob < ApplicationJob
   queue_as :default
 
-  #constant defined for SolidCP Resources
+  # constant defined for SolidCP Resources
   ADMIN_ROLE_ID = 1
-  RESELLER_ROLE_ID = 2 
+  RESELLER_ROLE_ID = 2
   USER_ROLE_ID = 3
   RESELLER_PLAN_ID = 10
-
 
   # Create user on SolidCP and Provision accounts
   # This job will create User Account on SolidCP and Provision the hosting Space for Reseller & User
   # static Plan Id 10 used for Reseller As of Now
   # FOr End User Real Plan ID will be Provided
 
-  def perform(user_id,product_id=nil)
+  def perform(user_id, product_id = nil)
     user = Spree::User.find(user_id)
     if user.store_admin? # reseller
-    	provision_store_admin_account(user,product_id)
+      provision_store_admin_account(user, product_id)
     else
-    	provision_user_account(user,product_id)
-    end 
+      provision_user_account(user, product_id)
+    end
   end
-
 
   private
 
-  def subscribe_to_solidcp_plan(user,plan_id)
-  	  user.solid_cp.package.add_package(plan_id) 
+  def subscribe_to_solidcp_plan(user, plan_id)
+    user.solid_cp.package.add_package(plan_id)
   end
 
   # this will provision the store Admin/Reseller Account and also creates hosting space for the reseller on solidcp panel
-  def provision_store_admin_account(user,product_id)
-    response = user.solid_cp.add_user('Reseller',RESELLER_ROLE_ID)
-    
-    solid_cp_master_plan_id = user.spree_store.solid_cp_master_plan_id 
-    if response[:success] == true
-     subscribe_to_solidcp_plan(user,solid_cp_master_plan_id) if user.packages.count <= 1
+  def provision_store_admin_account(user, product_id)
+    response = user.solid_cp.add_user('Reseller', RESELLER_ROLE_ID)
+
+    if product_id.present?
+      solid_cp_master_plan_id = user.spree_store.solid_cp_master_plan_id
+      if response[:success] == true
+        subscribe_to_solidcp_plan(user, solid_cp_master_plan_id) if user.packages.count <= 1
+      else
+        raise StandardError, response[:message]
+      end
     else
-      raise StandardError.new response[:message] 
+      Rails.logger.debug { "Not subscribing to any solidcp plan as plan id is nil" }
     end
   end
 
   # this will provision the store User Account and also creates hosting_space/Subscribe the plan on solidcp panel if product_id is passed is given
-  def provision_user_account(user,product_id)
-   response = user.solid_cp.add_user
-     if response[:success] == true
-       # this will only subscribe to plan if product id is given
-        if product_id.present?
-          product = Spree::Product.find(product_id)
-          if product.solid_cp_plan_id.present?  
-            subscribe_to_solidcp_plan(user,product.solid_cp_plan_id)
-          else
-            HostingPlanJob.perform(product_id)
-            raise StandardError.new "SolidCP Plan for this Product:##{product.id}-#{product.name} does not exist. HostingPlanJob started."
-          end 
+  def provision_user_account(user, product_id)
+    response = user.solid_cp.add_user
+    if response[:success] == true
+      # this will only subscribe to plan if product id is given
+      if product_id.present?
+        product = Spree::Product.find(product_id)
+        if product.solid_cp_plan_id.present?
+          subscribe_to_solidcp_plan(user, product.solid_cp_plan_id)
+        else
+          HostingPlanJob.perform(product_id)
+          raise StandardError,
+                "SolidCP Plan for this Product:##{product.id}-#{product.name} does not exist. HostingPlanJob started."
         end
-     else
-         raise StandardError.new response[:message] 
-     end
+      else
+        Rails.logger.debug { "Not subscribing to any solidcp plan as plan id is nil" }
+      end
+    else
+      raise StandardError, response[:message]
+    end
   end
-  
 end
--- app/services/app_manager/account_provisioner.rb
+++ app/services/app_manager/account_provisioner.rb
@@ -1,52 +1,50 @@
+# frozen_string_literal: true
+
 module AppManager
   class AccountProvisioner
-    
-    attr_reader :user,:product,:order
+    attr_reader :user, :product, :order
 
-      def initialize(user,options = {})
-        @user = user
-        @product = options[:product]
-        @order = options[:order]
+    def initialize(user, options = {})
+      @user = user
+      @product = options[:product]
+      @order = options[:order]
+    end
+
+    def call
+      Rails.logger.info { "AccountProvisioner is called " }
+      provison_solid_cp_account
+      provision_isp_config_account
+    end
+
+    def provison_solid_cp_account
+      if panels_access('solid_cp')
+        SolidCpProvisioningJob.set(wait: 3.second).perform_later(user.id, product&.id)
+        IspConfigProvisioningJob.set(wait: 3.second).perform_later(user.id, nil)
+        Rails.logger.info { "SolidCpProvisioningJob is scheduled to create user account on solid CP " }
       end
+    end
 
-      def call
-        Rails.logger.info { "AccountProvisioner is called " }
-        provison_solid_cp_account
-        provision_isp_config_account
+    def provision_isp_config_account
+      if panels_access('isp_config')
+        IspConfigProvisioningJob.set(wait: 3.second).perform_later(user.id, product&.id)
+        SolidCpProvisioningJob.set(wait: 3.second).perform_later(user.id, nil)
+        Rails.logger.info { "IspConfigProvisioningJob is scheduled to create user account on ISPConfig Account " }
       end
+    end
 
-      def provison_solid_cp_account
-        Rails.logger.info { "provison_solid_cp_account is called " }
+    private
 
-        if panels_access('solid_cp') 
-          SolidCpProvisioningJob.set(wait: 3.second).perform_later(user.id,product&.id) 
-          Rails.logger.info { "SolidCpProvisioningJob is scheduled to create user account on solid CP " }
+    def panels_access(panel)
+      @panels = []
+      if order.present?
+        order.line_items.each do |line_item|
+          next if line_item.product.blank?
+
+          panel_name = line_item.product.windows? ? 'solid_cp' : 'isp_config'
+          @panels << panel_name unless @panels.include?(panel)
         end
-
       end
-
-      def provision_isp_config_account
-        Rails.logger.info { "provision_isp_config_account is called " }
-
-        if panels_access('isp_config')
-          IspConfigProvisioningJob.set(wait: 3.second).perform_later(user.id,product&.id)
-          Rails.logger.info { "IspConfigProvisioningJob is scheduled to create user account on ISPConfig Account " }
-        end
-      end
-
-      private
-
-      def panels_access(panel)
-        @panels = []
-          if order.present?        
-              order.line_items.each do |line_item|
-                next if line_item.product.blank?
-                panel_name = line_item.product.windows? ? 'solid_cp' : 'isp_config' 
-                 @panels << panel_name  if !@panels.include?(panel)
-            end
-          end
-          @panels.include?(panel)
-      end
-
+      @panels.include?(panel)
+    end
   end
 end
