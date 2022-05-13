ff6206775be3715df4f32654cf62a2a621c135bf
Bhuvana101193 <bhuvaneshwari.uyyalam@apyl.com>
2022-05-13 14:31:03 +0530

    load error fix

--- app/lib/site_builder.rb
+++
@@ -1,6 +0,0 @@
-require "#{Rails.root}/app/lib/site_builder/base"
-require "#{Rails.root}/app/lib/site_builder/config"
-require "#{Rails.root}/app/lib/site_builder/site_builder"
-
-module SiteBuilder
-end
