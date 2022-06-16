574348e71d32a83a7878475a70d89e0dbdd8fd10
Syed Shibli <syed.m.shibli@et.apyl.com>
2022-06-16 09:49:51 +0000

    product listing Bug FIx

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -4,20 +4,11 @@ module Spree
   module Admin
     module ProductsControllerDecorator
       def collection
-        super
-        if TenantManager::TenantHelper.current_admin_tenant? && current_spree_user.store_admin?
-          @collection = begin
-            @collection.where(account_id: current_spree_user.account_id)
-          rescue StandardError
-            []
-          end
-        else
-          super
-        end
+        @collection = super
+        @collection = @collection.where(account_id: current_spree_user.account_id) rescue []
       end
 
       protected
-
       def location_after_save
         params.key?(:done) ? admin_products_path : edit_admin_product_url(@product)
       end
@@ -28,3 +19,4 @@ end
 if ::Spree::Admin::ProductsController.included_modules.exclude?(Spree::Admin::ProductsControllerDecorator)
   ::Spree::Admin::ProductsController.prepend Spree::Admin::ProductsControllerDecorator
 end
+
