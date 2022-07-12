43618db91a2318860e01b8b4702a2d811a4e7cfa
@stackexpress-anshita <anshita.gupta@apyl.com>
2022-07-12 19:44:35 +0530

    Dashboard Added

--- app/controllers/spree/admin/dashboard_controller.rb
+++ app/controllers/spree/admin/dashboard_controller.rb
@@ -1,4 +1,10 @@
-class Spree::Admin::DashboardController < ApplicationController
+module Spree
+    module Admin  
+class DashboardController < Spree::Admin::BaseController
+    
+
     def index
     end
+end
+end
 end
