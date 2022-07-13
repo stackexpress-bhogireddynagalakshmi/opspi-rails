89aa966a7fee623ab1ad4d874ea5a4a5bfddb4e0
@stackexpress-anshita <anshita.gupta@apyl.com>
2022-07-13 16:11:36 +0530

    Individual route added

--- config/routes.rb
+++ config/routes.rb
@@ -33,9 +33,7 @@ Rails.application.routes.draw do
         end
       end
 
-      resources :dashboard do 
-        
-      end
+      get 'dashboard', :controller=> 'dashboard',:action=>"index"
       
       resources :domain_registrations do 
         collection do
