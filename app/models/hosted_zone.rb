6546d0ae7e5526e1fe463001c062500057844aa0
Syed Shibli <syed.m.shibli@et.apyl.com>
2022-07-26 17:04:01 +0530

    Hosted Zone

--- app/models/hosted_zone.rb
+++ app/models/hosted_zone.rb
@@ -3,7 +3,7 @@
 class HostedZone < ApplicationRecord
   belongs_to :user, class_name: 'Spree::User'
   has_many   :hosted_zone_records
-  belongs_to :user_domain, dependent: :destroy
+  belongs_to :user_domain, dependent: :destroy, optional: :true
 
   after_commit :ensure_user_domain_updated, on: [:create]
 
