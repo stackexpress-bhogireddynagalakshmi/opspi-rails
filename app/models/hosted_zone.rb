27aeaff2c0a7e130d6456dfd646703ca14a8baac
Syed Shibli <syed.m.shibli@et.apyl.com>
2022-07-26 17:21:08 +0530

    logic updated

--- app/models/hosted_zone.rb
+++ app/models/hosted_zone.rb
@@ -3,15 +3,5 @@
 class HostedZone < ApplicationRecord
   belongs_to :user, class_name: 'Spree::User'
   has_many   :hosted_zone_records
-  belongs_to :user_domain, dependent: :destroy, optional: :true
-
-  after_commit :ensure_user_domain_updated, on: [:create]
-
-  def ensure_user_domain_updated
-    user_domain = user.user_domains.where(domain: name).last
-    return nil if user_domain.blank?
-
-    user_domain.update_column(:success, true)
-    self.update_column(:user_domain_id, user_domain.id)
-  end
+  belongs_to :user_domain, dependent: :destroy
 end
