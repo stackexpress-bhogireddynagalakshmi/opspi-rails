55b942e8517d7cb942f29f709a398856ea5dfe9c
@stackexpress-anshita <anshita.gupta@apyl.com>
2022-08-10 10:49:52 +0530

    Reverted to Old UI

--- app/controllers/spree/admin/my_account_invoices_controller.rb
+++ app/controllers/spree/admin/my_account_invoices_controller.rb
@@ -1,8 +1,18 @@
 module Spree
     module Admin
 class MyAccountInvoicesController < Spree::Admin::BaseController
+    before_action :layout_type
+    
     def index
         @invoices = TenantManager::TenantHelper.unscoped_query { current_spree_user.invoices.order(created_at: :desc) }
+    end
+
+    def layout_type
+        if current_spree_user.store_admin?
+            render layout: "spree/layouts/admin"
+          elsif current_spree_user&.superadmin?
+            render layout: "spree/layouts/admin"
+          end
     end
 end
 end
--- app/controllers/spree/admin/my_account_subscriptions_controller.rb
+++ app/controllers/spree/admin/my_account_subscriptions_controller.rb
@@ -1,7 +1,17 @@
 module Spree
 module Admin
 class MyAccountSubscriptionsController < Spree::Admin::BaseController
+    before_action :layout_type
+    
     def index
+    end
+
+    def layout_type
+        if current_spree_user.store_admin?
+            render layout: "spree/layouts/admin"
+          elsif current_spree_user&.superadmin?
+            render layout: "spree/layouts/admin"
+          end
     end
 end
 end
