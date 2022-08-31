8941677e3f523358f10392c8f47c4accef680fc6
@stackexpress-anshita <anshita.gupta@apyl.com>
2022-08-31 23:17:46 +0530

    Removing Confirmation process

--- app/controllers/spree/user_registrations_controller_decorator.rb
+++ app/controllers/spree/user_registrations_controller_decorator.rb
@@ -4,12 +4,43 @@ module Spree
   module UserRegistrationsControllerDecorator
     # POST /resource/sign_up
     def create
-      super
+    
+
+    @user = build_resource(spree_user_params)
+    resource_saved = resource.save
+
+    yield resource if block_given?
+    
+    if resource_saved
+        
+        if  request.url == '/checkout/registration'
+          sign_in(resource_name, resource)
+          session[:spree_user_session] = true
+
+        flash[:success] = Spree.t(:send_instructions)
+
+        redirect_to spree.checkout_state_path(:address)
+        else
+
+          resource.send_confirmation_instructions(current_store) if Spree::Auth::Config[:confirmable]
+          flash[:success] = Spree.t(:send_instructions)
+
+          redirect_to '/login'
+        end
+
+    else
+      # flash[:error] = resource.inactive_message 
+    end
+
       if resource.persisted?
         order = Spree::Order.find_by_token(cookies.signed[:token])
         order.update_column(:user_id, resource.id) if order.present? && order.user_id.blank?
       end
-    end
+      
+  end
+
+
+
   end
 end
 
