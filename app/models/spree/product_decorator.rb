4be688f9a384c427815ad09c90d4ae56b7c60dec
Bhuvana101193 <bhuvaneshwari.uyyalam@apyl.com>
2022-09-27 11:06:21 +0530

    MR fix

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -44,12 +44,9 @@ module Spree
     def set_validity
       self.validity = 1
     end
+
     def create_product_config
-<<<<<<< HEAD
-      ProductConfig.new(self,isp_config_limit,plan_quotas).call
-=======
       AppManager::ProductConfigCreator.new(self.as_json,isp_config_limit.as_json,plan_quotas.as_json).call
->>>>>>> create product config on product create
     end
     
     def ensure_plan_id_or_template_id
@@ -116,7 +113,6 @@ end
 ::Spree::Product.prepend Spree::ProductDecorator if ::Spree::Product.included_modules.exclude?(Spree::ProductDecorator)
 
 [:plan_type, :server_type, :solid_cp_master_plan_id, :isp_config_master_template_id, :subscribable, :reseller_product, :no_of_website, :storage, :ssl_support, :domain, :subdomain, :parked_domain, :mailbox, :auto_daily_malware_scan, :email_order_confirmation, :frequency, :validity, :description, :visible,
->>>>>>> changes
  { isp_config_limit_attributes: IspConfigLimit.get_fields_name,
    plan_quota_groups_atrributes: [:group_name, :product_id, :solid_cp_quota_group_id, :calculate_diskspace, :calculate_bandwidth, :enabled, :id,
                                   { plan_quotas_attributes: %i[quota_name plan_quota_group_id solid_cp_quota_group_id solid_cp_quota_id quota_value unlimited enabled parent_quota_value id quota_type_id] }] }].each do |field|
