017c2f45b5f818875f0a4993108eb8c97dd2a84c
Syed Shibli <syed.m.shibli@et.apyl.com>
2022-09-29 10:34:02 +0530

    Backup for ISP config completed

--- app/controllers/spree/admin/sites/backup_controller.rb
+++ app/controllers/spree/admin/sites/backup_controller.rb
@@ -1,10 +1,26 @@
 class Spree::Admin::Sites::BackupController < ApplicationController
   before_action :set_user_domain
-  before_action :set_website
 
   def update
-     @website = @user_domain.user_website
-     backup_api.update(@website.id, {})
+    @website = @user_domain.user_website
+    if params[:user_website][:enable_backup] == '1' 
+      backup_api.start_backup(@website.id)
+    else
+      backup_api.stop_backup(@website.id)
+    end
+  end
+
+  def index
+    @website = @user_domain.user_website
+    result = backup_api.all(@website.id)
+
+    if result[:success] == true
+      @backups = result[:response].response
+    else
+      @backups = []
+    end
+
+    render :partial=> 'index',locals: {backups: @backups }, layout: false
   end
 
   private
@@ -12,11 +28,8 @@ class Spree::Admin::Sites::BackupController < ApplicationController
   def backup_api
     if @user_domain.windows?
       #TODO yet to implement
-
     else
       current_spree_user.isp_config.site_backup
     end
   end
-
-
 end
--- app/lib/isp_config/site_backup.rb
+++ app/lib/isp_config/site_backup.rb
@@ -9,8 +9,23 @@ module IspConfig
       set_base_uri( user.panel_config["web_linux"] )
     end
 
+
+    def all(id)
+      user_website = UserWebsite.find(id)
+      response = query({
+                         endpoint: '/json.php?sites_web_domain_backup_list',
+                         method: :GET,
+                         body: {
+                           primary_id: user_website.remote_website_id
+                         }
+                       })
+
+
+      formatted_response(response, 'list')
+    end
+
     
-    def update(id, params = {})
+    def start_backup(id, params = {})
       user_website = UserWebsite.find(id)
       response = query({
                          endpoint: '/json.php?sites_web_domain_update',
@@ -18,20 +33,71 @@ module IspConfig
                          body: {
                            client_id: user.isp_config_id,
                            primary_id: user_website.remote_website_id,
-                           params: params.merge(backup_Params)
+                           params: params.merge(start_backup_Params)
                          }
                        })
 
-      formatted_response(response, 'update')
+      if response.code == 'ok'
+        user_website.update(enable_backup: true)
+      end
+
+      formatted_response(response, 'start')
     end
 
-    def backup_Params
+    def stop_backup(id, params = {})
+      user_website = UserWebsite.find(id)
+      response = query({
+                         endpoint: '/json.php?sites_web_domain_update',
+                         method: :POST,
+                         body: {
+                           client_id: user.isp_config_id,
+                           primary_id: user_website.remote_website_id,
+                           params: params.merge(stop_backup_Params)
+                         }
+                       })
+
+      if response.code == 'ok'
+        user_website.update(enable_backup: false)
+      end
+
+      formatted_response(response, 'stop')
+    end
+
+    private
+
+    def start_backup_Params
       {
-        backup_interval: 'daily',
-        backup_copies: 5,
-        backup_format_web: 'default',
-        backup_format_db: 'gzip',
+        backup_interval:    'daily',
+        backup_copies:       5,
+        backup_format_web:  'default',
+        backup_format_db:   'gzip',
       }
     end
+
+    def stop_backup_Params
+      {
+        backup_interval:    '',
+        backup_copies:       1,
+        backup_format_web:  'default',
+        backup_format_db:   'gzip',
+      }
+    end
+
+    def formatted_response(response, action)
+      if  response.code == "ok"
+        {
+          success: true,
+          message: I18n.t("backup.#{action}"),
+          response: response
+        }
+      else
+        {
+          success: false,
+          message: I18n.t('isp_config.something_went_wrong', message: response.message),
+          response: response
+        }
+      end
+    end
+
   end
 end
