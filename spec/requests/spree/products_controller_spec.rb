2cf97300cd0e6f265d76e94b63867a22908aa0ed
Bhuvana101193 <bhuvaneshwari.uyyalam@apyl.com>
2022-10-25 15:55:50 +0530

    product controller spec

--- spec/requests/spree/products_controller_spec.rb
+++ spec/requests/spree/products_controller_spec.rb
@@ -10,31 +10,44 @@ RSpec.describe Spree::Admin::ProductsController, type: :controller do
   let(:admin_store) {create(:spree_store,url: 'example.com')}
 
 
-  # context "store [example.com]" do
-  #   before(:each) do
-  #     @request.host = admin_store.url #example.com
-  #   end
+  context "store [example.com]" do
+    before(:each) do
+      @request.host = admin_store.url #example.com
+    end
 
-  #   context '#create session for super admin' do
-  #     it 'using correct login information on admin store' do
-  #       post :create, params: { spree_user: { email: admin_user.email ,password: 'opspi@123'} }
-  #       expect(flash[:error]).to eq(nil)
-  #       # expect(flash[:success]).to eq("Logged in successfully")
-  #     end
-  #   end
+    context '#create session for super admin' do
+      it 'using correct login information on admin store' do
+        post :create, params: { spree_user: { email: admin_user.email ,password: 'opspi@123'} }
+        expect(flash[:error]).to eq(nil)
+        expect(response.status).to eq(302)
+        # expect(flash[:success]).to eq("Logged in successfully")
+      end
+    end
 
+    context '#crud  for super admin' do
+      it "creates a Product" do
+        post :create, params: { spree_product: { name: "nsbsb" ,available_on: Date.today, server_type: "reseller_plan", price: 100, account_id: admin_store.account.id} }
+        expect(flash[:error]).to eq(nil)
+        expect(response.status).to eq(302)
+      end
 
-  #   it 'payment capture by super admin' do
-  #     byebug
-  #     # put :fire, params: { payment: {e: "capture", order_id: , id: }}
-  #   end
+      it "list products" do
+        get :index
+        expect(response.status).to eq(302)
+        # expect(response).to have_http_status(:success)
+        # expect(flash[:success]).to eq("")  
+      end
 
-  # end
+      # it "Delete a product" do
+      #   delete :destroy, params: { spree_product: { id: ""}}
+      # end
 
-  it "creates a Product and redirects to the Product index page" do
-    post :create, params: { spree_product: { name: "nsbsb" ,available_on: Date.today, server_type: "reseller_plan", price: 100, account_id: admin_store.account.id} }
-    expect(flash[:error]).to eq(nil)
-    # expect(flash[:success]).to eq("")  
+      # it "updates a Product" do
+      #   put :update, params: { spree_product: { name: "nsbsb" ,available_on: Date.today, server_type: "reseller_plan", price: 110, account_id: admin_store.account.id} }
+      #   expect(flash[:error]).to eq(nil)
+      #   expect(response.status).to eq(302)
+      # end
+    end
   end
 
 end
