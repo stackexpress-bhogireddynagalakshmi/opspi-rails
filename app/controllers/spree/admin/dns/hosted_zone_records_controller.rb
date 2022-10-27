c77d97233c7529dcc50fe37e3712c958abb40139
Syed Shibli <syed.m.shibli@et.apyl.com>
2022-10-27 10:40:13 +0530

    Host zone file reverted

--- app/controllers/spree/admin/dns/hosted_zone_records_controller.rb
+++ app/controllers/spree/admin/dns/hosted_zone_records_controller.rb
@@ -6,10 +6,10 @@ module Spree
       class HostedZoneRecordsController < Spree::Admin::BaseController
         before_action :ensure_hosting_panel_access
         before_action :set_hosted_zone
-        before_action :set_user_domain, only: %i[create update destroy]
+        before_action :set_user_domain, only: [:create, :update, :destroy]
 
         def create
-          @response = host_zone_record_api.create(host_zone_record_params)
+          @response = host_zone_record_api.create(host_zone_record_params) 
           set_flash
           # redirect_to dns_admin_dns_hosted_zone_url(@hosted_zone.isp_config_host_zone_id)+"?dns_name=#{host_zone_record_params[:hosted_zone_name]}"
           # redirect_to "/admin/dns/hosted_zones/zone_overview?zone_name=#{host_zone_record_params[:hosted_zone_name]}&dns_id=#{@hosted_zone.isp_config_host_zone_id}&user_domain_id=#{host_zone_record_params[:user_domain_id]}"
@@ -20,21 +20,12 @@ module Spree
           @response = host_zone_record_api.update(host_zone_record_params)
           set_flash
           # redirect_to dns_admin_dns_hosted_zone_url(@hosted_zone.isp_config_host_zone_id)+"?dns_name=#{host_zone_record_params[:hosted_zone_name]}"
-          # redirect_to "/admin/dns/hosted_zones/zone_overview?zone_name=#{host_zone_record_params[:hosted_zone_name]}&dns_id=#{@hosted_zone.isp_config_host_zone_id}"
+          #redirect_to "/admin/dns/hosted_zones/zone_overview?zone_name=#{host_zone_record_params[:hosted_zone_name]}&dns_id=#{@hosted_zone.isp_config_host_zone_id}"
           redirect_to request.referrer
         end
 
         def destroy
-          res1 = host_zone_record_api.destroy(host_zone_record_params)
-
-          res2 = website_api.destroy(@user_domain.user_website.id)
-
-          # stepo 1 Delete Wev=bsite Domain
-          # Step 2  Delete Mail Domain
-          # Step 3 Delete FTP Users
-          # step 4 Delete Database and Database users
-          # Step Mail FOrwards/mailing list/spam filters/
-
+          @response = host_zone_record_api.destroy(host_zone_record_params)
           set_flash
           respond_to do |format|
             format.js { render inline: "location.reload();" }
@@ -57,26 +48,10 @@ module Spree
           current_spree_user.isp_config.hosted_zone_record
         end
 
-        def website_api
-          if @user_domain.linux?
-            current_spree_user.isp_config.website
-          else
-            current_spree_user.solid_cp.web_domain
-          end
-        end
-
-        def ftp_user_api
-          if @user_domain.linux?
-            current_spree_user.isp_config
-                              .else
-            current_spree_user.solid_cp.ftp_user
-          end
-        end
-
         def host_zone_record_params
           params.permit(:name, :type, :ipv4, :ipv6, :publickey, :dkim, :target, :mailserver, :priority, :nameserver, :content,
                         :hosted_zone_id, :ttl, :id, :client_id, :primary_id, :hosted_zone_name, :user_domain_id).merge!({ hosted_zone_id: @hosted_zone.isp_config_host_zone_id,
-                                                                                                                          client_id: current_spree_user.isp_config_id })
+                                                                                      client_id: current_spree_user.isp_config_id })
         end
 
         def set_hosted_zone
